071306ec5b31b74c73889e80d5d79f7fb18d3e84
eva.wang <eva.wang81@gmail.com>
2022-03-31 08:44:33 +0800

    clean up code

--- script.js
+++ script.js
@@ -17,9 +17,6 @@ let firstCardElement;
 let deck;
 
 const squareClick = (cardElement, row, column) => {
-  console.log(cardElement);
-  console.log("FIRST CARD DOM ELEMENT", firstCard);
-  console.log("BOARD CLICKED CARD", board[row][column]);
   const clickedCard = board[row][column];
   // the user already clicked on this square
   if (cardElement.innerText !== "") {
@@ -57,62 +54,40 @@ const squareClick = (cardElement, row, column) => {
 // create all the board elements that will go on the screen
 // return the built board
 const buildBoardElements = (board) => {
-  // create the element that everything will go inside of
+  console.log(board);
   const boardElement = document.createElement("div");
-
-  // give it a class for CSS purposes
   boardElement.classList.add("board");
-
-  // use the board data structure we passed in to create the correct size board
   for (let i = 0; i < board.length; i += 1) {
-    // make a var for just this row of cards
     const row = board[i];
-
-    // make an element for this row of cards
     const rowElement = document.createElement("div");
     rowElement.classList.add("row");
-
-    // make all the squares for this row
     for (let j = 0; j < row.length; j += 1) {
-      // create the square element
       const square = document.createElement("div");
-
-      // set a class for CSS purposes
       square.classList.add("square");
-
-      // set the click event
-      // eslint-disable-next-line
       square.addEventListener("click", (event) => {
         // we will want to pass in the card element so
         // that we can change how it looks on screen, i.e.,
         // "turn the card over"
         squareClick(event.currentTarget, i, j);
       });
-
       rowElement.appendChild(square);
     }
     boardElement.appendChild(rowElement);
   }
-
   return boardElement;
 };
 
-const makeDeck = (cardAmount) => {
-  // create the empty deck at the beginning
+const makeDeck = () => {
   const newDeck = [];
   const suits = ["hearts", "diamonds", "clubs", "spades"];
-
+  const suitsSymbol = ["♥︎", "♦︎", "♣︎", "♠︎"];
   for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
-    // make a variable of the current suit
     const currentSuit = suits[suitIndex];
-    console.log(`current suit: ${currentSuit}`);
-
-    // loop to create all cards in this suit
+    const currentSymbol = suitsSymbol[suitIndex];
     // rank 1-13
     for (let rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
       // Convert rankCounter to string
       let cardName = `${rankCounter}`;
-
       // 1, 11, 12 ,13
       if (cardName === "1") {
         cardName = "ace";
@@ -123,33 +98,40 @@ const makeDeck = (cardAmount) => {
       } else if (cardName === "13") {
         cardName = "king";
       }
-
       // make a single card object variable
       const card = {
         name: cardName,
         suit: currentSuit,
+        suitSymbol: currentSymbol,
         rank: rankCounter,
       };
-
-      console.log(`rank: ${rankCounter}`);
-
       // add the card to the deck
-      newDeck.push(card); // add double the cards to the deck
+      newDeck.push(card);
       newDeck.push(card);
     }
   }
-
   return newDeck;
 };
 
+// Shuffle an array of cards
+const getRandomIndex = (max) => Math.floor(Math.random() * max);
+const shuffleCards = (cards) => {
+  for (let currentIndex = 0; currentIndex < cards.length; currentIndex += 1) {
+    const randomIndex = getRandomIndex(cards.length);
+    const randomCard = cards[randomIndex];
+    const currentCard = cards[currentIndex];
+    cards[currentIndex] = randomCard;
+    cards[randomIndex] = currentCard;
+  }
+  // Return the shuffled deck
+  return cards;
+};
+
+// Make a 4x4 board with cards but don't show content in html
 const initGame = () => {
-  // create this special deck by getting the doubled cards and
-  // making a smaller array that is ( boardSize squared ) number of cards
-  let doubleDeck = makeDeck();
+  const doubleDeck = makeDeck();
   let deckSubset = doubleDeck.slice(0, boardSize * boardSize);
   deck = shuffleCards(deckSubset);
-
-  // deal the cards out to the board data structure
   for (let i = 0; i < boardSize; i += 1) {
     board.push([]);
     for (let j = 0; j < boardSize; j += 1) {
@@ -159,3 +141,5 @@ const initGame = () => {
   const boardEl = buildBoardElements(board);
   document.body.appendChild(boardEl);
 };
+
+initGame();
